b9c7d546b73c43d7570117d6be4cf0544e4bc981
goraider <alejandro_gosain@hotmail.com>
2022-09-21 19:40:15 -0500

    se agrego el formato del id en el modelo de usuarios

--- models/Usuario.js
+++ models/Usuario.js
@@ -17,4 +17,11 @@ const UsuarioSchema =  Schema({
     }
 });
 
+UsuarioSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+
+});
+
 module.exports = model( 'Usuario',UsuarioSchema );
